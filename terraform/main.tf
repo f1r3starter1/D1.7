d5cbb254740b21fb5f3c11df6231b387bfd0d840
f1r3starter1 <93195717+f1r3starter1@users.noreply.github.com>
2023-04-03 17:01:59 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 provider "yandex" {
-  token     = "t1.9euelZrGlZ6ZzI-Jns6Vi82enp6VkO3rnpWaxpGelMzHnY3JksvNlInLjczl9Pd1GVZe-e9QNGOr3fT3NUhTXvnvUDRjqw.DxfA-nNU6nXDQyXS6nBckCzZdJmyPIWgP_zyJzpYPeBr6Lj1LoW9YZ7dEAj1ODUChs2eQ_-5GXLna0K5cPSkCw"
-  cloud_id  = "b1g52fls4gn42ltpb8r4"
-  folder_id = "b1gkah722jo1350jlth2"
+  token     = ""
+  cloud_id  = ""
+  folder_id = ""
   zone      = "ru-central1-a"
 }
 
